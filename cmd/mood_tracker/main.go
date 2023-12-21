d65dd4e61b04a65e011a4c59db683deacd7f8c6c
Alexandr Demidov <alexander.demidov.1997@gmail.com>
2023-12-21 21:49:37 +0300

    initial commit with config by cleanenv

---
+++ cmd/mood_tracker/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"mood_tracker/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+}
---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env        string `yaml:"env" env:"ENV" env-default:"prod"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH isn't set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file doesn't exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
