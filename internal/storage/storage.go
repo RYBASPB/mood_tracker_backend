e9936ce1dd04e96a8f0684fd1358001018b76742
Alexandr Demidov <alexander.demidov.1997@gmail.com>
2024-10-07 01:39:02 +0300

    add database entities and DTOs

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,24 @@
 package storage
+
+import "time"
+
+type MoodScore struct {
+	Id     int64     `db:"id"`
+	Score  int8      `db:"score"`
+	Date   time.Time `db:"date"`
+	UserId int64     `db:"user_id"`
+}
+
+type AddMoodScoreDto struct {
+	Score  int64
+	UserId int64
+}
+
+type User struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+
+type CreateUserDto struct {
+	Name string
+}
