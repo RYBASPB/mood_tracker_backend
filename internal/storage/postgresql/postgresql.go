12b4638cd50da94593a2ed571bece035eda22898
Alexandr Demidov <alexander.demidov.1997@gmail.com>
2024-10-07 01:39:43 +0300

    add methods for creating user and mood score, getting mood score

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -3,7 +3,10 @@ package postgresql
 import (
 	"context"
 	"fmt"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"mood_tracker/internal/storage"
 	"os"
 )
 
@@ -19,14 +22,64 @@ func ConnectToDB() (*Storage, error) {
 		return nil, fmt.Errorf("%s : %w", op, err)
 	}
 
-	_, err = pool.Exec(context.Background(), `
-		create table if not exists mood_tracker(
-		    id integer primary key,
-		    score integer not null);
-	`)
-	if err != nil {
-		return nil, fmt.Errorf("%s : %w", op, err)
-	}
-
 	return &Storage{pool}, err
+}
+
+func (s *Storage) AddMoodScore(dto storage.AddMoodScoreDto) (moodScoreId int64, err error) {
+	const op = "storage.postgresql.AddMoodScore"
+	sql, args, err := sq.
+		Insert("mood_scores").
+		Columns("user_id", "score").
+		Values(dto.UserId, dto.Score).
+		Suffix("RETURNING \"id\"").
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return 0, fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+	queryResult := s.db.QueryRow(context.Background(), sql, args...)
+
+	if err := queryResult.Scan(&moodScoreId); err != nil {
+		return 0, fmt.Errorf("%s: scan: %w", op, err)
+	}
+	return
+}
+
+func (s *Storage) GetMoodScoresById(userId int64) (moodScores []storage.MoodScore, err error) {
+	const op = "storage.postgresql.GetMoodScoresById"
+	sql, args, err := sq.
+		Select("id", "score", "date", "user_id").
+		From("mood_scores").
+		Where("user_id = ?", userId).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	rows, err := s.db.Query(context.Background(), sql, args...)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed query: %w", op, err)
+	}
+	moodScores, err = pgx.CollectRows(rows, pgx.RowToStructByName[storage.MoodScore])
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed collecting rows: %w", op, err)
+	}
+	return
+}
+
+func (s *Storage) AddUser(dto storage.CreateUserDto) (userId int64, err error) {
+	const op = "storage.postgresql.AddUser"
+	sql, args, err := sq.
+		Insert("users").
+		Columns("name").
+		Values(dto.Name).
+		Suffix("RETURNING \"id\"").
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return 0, fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+	queryResult := s.db.QueryRow(context.Background(), sql, args...)
+	err = queryResult.Scan(&userId)
+	if err != nil {
+		return 0, fmt.Errorf("%s: scan: %w", op, err)
+	}
+	return
 }
