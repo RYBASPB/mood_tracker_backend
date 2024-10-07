52fd289e532951b03c9bb2ccb9e667560388d8ac
Alexander <alexander.demidov.1997@gmail.com>
2024-10-07 13:35:58 +0300

    create http server, add dependencies

--- cmd/mood_tracker/main.go
+++ cmd/mood_tracker/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"mood_tracker/internal/config"
+	"mood_tracker/internal/http-server/handlers/score/add"
+	httpLogger "mood_tracker/internal/http-server/middleware/logger"
 	"mood_tracker/internal/storage/postgresql"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 const (
@@ -22,7 +28,7 @@ func main() {
 	log.Info("starting logger", slog.String("env", cfg.Env))
 	log.Debug("log level is Debug")
 
-	_, err := postgresql.ConnectToDB()
+	db, err := postgresql.ConnectToDB()
 	if err != nil {
 		log.Error("Error in initialization of DB", slog.AnyValue(err))
 	}
@@ -35,6 +41,46 @@ func main() {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
+
+	router.Use(middleware.Timeout(60 * time.Second))
+	router.Use(httpLogger.New(log))
+
+	router.Route("/scores", func(r chi.Router) {
+		r.Post("/", add.New(log, db))
+	})
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	server := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.ReadTimeout,
+		WriteTimeout: cfg.HTTPServer.WriteTimeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Error("error in starting server", slog.AnyValue(err))
+		}
+	}()
+	log.Info("server started")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.HTTPServer.ContextTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("error in shutting down", slog.AnyValue(err))
+		return
+	}
+	log.Info("server stopped")
+
+	db.CloseConnection()
+	log.Info("db connection closed")
 }
 
 func setupLogger(env string) *slog.Logger {
